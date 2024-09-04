050e90ad581ff84eeab8765858234af25237c5e1
Roaa2002-web <roaa.abdulfattah.alwesabi@gmail.com>
2024-09-04 09:21:04 +0300

    Initial commit

---
+++ src/components/Filter.js
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+
+function Filter({ onCategoryChange, onSearchChange }) {
+  const [searchText, setSearchText] = useState("");
+
+  function handleSearchChange(event) {
+    const newSearchText = event.target.value;
+    setSearchText(newSearchText);
+    onSearchChange(newSearchText);
+  }
+
+  return (
+    <div className="Filter">
+      <input 
+        type="text" 
+        name="search" 
+        placeholder="Search..." 
+        value={searchText}
+        onChange={handleSearchChange}
+      />
+      <select name="filter" onChange={onCategoryChange}>
+        <option value="All">Filter by category</option>
+        <option value="Produce">Produce</option>
+        <option value="Dairy">Dairy</option>
+        <option value="Dessert">Dessert</option>
+      </select>
+    </div>
+  );
+}
+
+export default Filter;
